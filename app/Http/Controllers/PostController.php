e85862bf5c8f5d8767a21e04455c1db92b7c1463
revTec2022_elly <eri.suzuki2024@gmail.com>
2022-07-14 19:43:30 +0900

    カリキュラム8-2の課題4完了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,6 @@ use App\Post; /* Postモデルクラスの定義 */
 
 class PostController extends Controller{
     public function index(Post $post){
-        return view("posts/index")->with(["posts" => $post->getByLimit()]);
+        return view("posts/index")->with(["posts" => $post->getPaginateByLimit()]);
     }
 }
